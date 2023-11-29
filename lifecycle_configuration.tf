3609efbb44a1b4c9f0609aa17aee98c883b5f0e1
Hermann Benda <herlbeng@hotmail.com>
2023-11-29 12:18:59 -0600

    Inicio sagemaker scripts

---
+++ lifecycle_configuration.tf
@@ -0,0 +1,6 @@
+# Defining the SageMaker notebook lifecycle configuration
+resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "notebook_config" {
+  name = "sntndr-sagemaker-lifecycle-config"
+  on_create = filebase64("../scripts/on-create.sh")
+  on_start = filebase64("../scripts/on-start.sh")
+}
