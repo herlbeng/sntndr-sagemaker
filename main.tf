c26f512e7c8aff063e847f0097bedc16ea42c932
Hermann Benda <herlbeng@hotmail.com>
2023-12-13 13:47:13 -0600

    Cambio de nombres de los recursos con project_name2

--- main.tf
+++ main.tf
@@ -2,29 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "<= 5.26.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "sgmkr-tf-mlops"
-    key    = "sgmkr-tf-mlops.tfstate"
-    region = "us-east-1"
-  }
-
-  required_version = ">= 1.6.0"
-}
-
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 data "aws_iam_policy_document" "sagemaker_assume_role" {
   statement {
     actions = ["iam:CreateRole"]
@@ -41,12 +18,6 @@ resource "aws_iam_role" "sagemaker_domain_role" {
   assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
 }
 
-resource "aws_iam_role" "sagemaker_exec_role" {
-  for_each           = var.user_profile_names
-  name               = "${var.project_name}_${each.key}_exec-role"
-  assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
-}
-
 resource "aws_sagemaker_domain" "sagemakerDomainPOC" {
   domain_name = "${project_name}-domain"
   auth_mode   = "IAM"
@@ -55,18 +26,5 @@ resource "aws_sagemaker_domain" "sagemakerDomainPOC" {
 
   default_user_settings {
     execution_role = aws_iam_role.sagemaker_domain_role.arn
-  }
-}
-
-resource "aws_sagemaker_user_profile" "mlops_profile" {
-  for_each          = var.user_profile_names
-  domain_id         = aws_sagemaker_domain.sagemakerDomainPOC.id #"d-0v5zdh7zffd0" #"d-wxhuu1mbq9pm" #"d-s8hloa35njwk"
-  user_profile_name = each.key
-
-  user_settings {
-    execution_role = aws_iam_role.sagemaker_exec_role[each.key].arn
-  }
-  tags = {
-    Name = "mlops"
   }
 }
