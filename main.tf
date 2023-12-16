b7fc4fabf2c9a0ff2338a279e0f5e8871fb308fa
Hermann Benda <herlbeng@hotmail.com>
2023-12-15 20:25:58 -0600

    Acceso6 netw

--- main.tf
+++ main.tf
@@ -18,6 +18,12 @@ resource "aws_iam_role" "sagemaker_domain_role" {
   assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
 }
 
+resource "aws_iam_role" "sagemaker_exec_role" {
+  for_each           = var.user_profile_names
+  name               = "${var.project_name}_${each.key}_exec-role"
+  assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
+}
+
 resource "aws_sagemaker_domain" "sagemakerDomain" {
   domain_name = "${var.project_name}-domain"
   auth_mode   = "IAM"
@@ -26,5 +32,18 @@ resource "aws_sagemaker_domain" "sagemakerDomain" {
 
   default_user_settings {
     execution_role = aws_iam_role.sagemaker_domain_role.arn
+  }
+}
+
+resource "aws_sagemaker_user_profile" "mlops_profile" {
+  for_each          = var.user_profile_names
+  domain_id         = aws_sagemaker_domain.sagemakerDomain.id #"d-0v5zdh7zffd0" #"d-wxhuu1mbq9pm" #"d-s8hloa35njwk"
+  user_profile_name = each.key
+
+  user_settings {
+    execution_role = aws_iam_role.sagemaker_exec_role[each.key].arn
+  }
+  tags = {
+    Name = "mlops"
   }
 }
