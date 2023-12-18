a727911e167603276e7f21ad89284eecfd0a5f39
Hermann Benda <herlbeng@hotmail.com>
2023-12-18 13:40:30 -0600

    Acceso12 cambio de role

--- notebook_instance.tf
+++ notebook_instance.tf
@@ -4,6 +4,6 @@ resource "aws_sagemaker_notebook_instance" "notebook_instance" {
   #role_arn = aws_iam_role.notebook_iam_role.arn
   role_arn                = aws_iam_role.sagemaker_exec_role["data-science"]
   instance_type           = "ml.t2.medium"
-  lifecycle_config_name   = aws_sagemaker_notebook_instance_lifecycle_configuration.notebook_config.name
-  default_code_repository = aws_sagemaker_code_repository.git_repo.code_repository_name
+  #lifecycle_config_name   = aws_sagemaker_notebook_instance_lifecycle_configuration.notebook_config.name
+  #default_code_repository = aws_sagemaker_code_repository.git_repo.code_repository_name
 }
