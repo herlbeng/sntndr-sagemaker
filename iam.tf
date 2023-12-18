2b0ecf8a85b5c9d016559bd455045b034b701026
Hermann Benda <herlbeng@hotmail.com>
2023-12-18 13:46:47 -0600

    Acceso13 cambio de iam

--- iam.tf
+++ iam.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "sm_assume_role_policy" {
 # Defining the SageMaker notebook IAM role
 resource "aws_iam_role" "notebook_iam_role" {
   name               = "${var.project_name}-sm-notebook-role"
-  assume_role_policy = data.aws_iam_policy_document.sm_assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
 }
 
 # Attaching the AWS default policy, "AmazonSageMakerFullAccess"
